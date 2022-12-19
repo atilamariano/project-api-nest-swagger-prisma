e9d05499497533f9c23c96d26624081069549fba
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2022-12-19 11:25:08 -0300

    feat(prisma): module & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
 import { GenreModule } from './genre/genre.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-    imports: [UsersModule, BooksModule, GenreModule],
-    controllers: [],
-    providers: [],
+   imports: [UsersModule, BooksModule, GenreModule, PrismaModule],
+   controllers: [],
+   providers: [],
 })
 export class AppModule {}
